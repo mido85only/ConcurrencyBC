016265e16befda30efb6fe8a8465e05be0a45beb
Mohamed Aziz <aziz@corptia.com>
2023-07-27 11:44:48 +0300

    Initial Commit

---
+++ ConcurrencyBC/ConcurrencyBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyBCApp.swift
+//  ConcurrencyBC
+//
+//  Created by Corptia on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
